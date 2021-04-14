f2f6d8d40fac611d094b66c5b7e1a07ddc56310f
ekoziol-bain <eric.koziol@bain.com>
2021-04-14 11:17:21 -0400

    created parser class

--- ssg/site.py
+++ ssg/site.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
@@ -15,3 +16,20 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    def load_parser(self, extension):
+        for p in self.parsers:
+            if p.valid_extension(extension):
+                return p
+
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("Not Implemented")
+
+
