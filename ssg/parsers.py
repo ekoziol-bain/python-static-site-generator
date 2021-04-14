5be97e17924ed2c98b4e1668bd6e4d30bc5f4d6d
ekoziol-bain <eric.koziol@bain.com>
2021-04-14 11:26:01 -0400

    module 2 weird errors fixed

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,8 @@
+import shutil
+
 from typing import List
 from pathlib import Path
-import shutil
+
 
 class Parser:
     extensions: List[str] = []
@@ -11,21 +13,21 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-    def read(self. path):
+    def read(self, path):
         with open(path, "r") as file:
             return file.read()
 
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-
         with open(full_path, "w") as file:
             file.write(content)
 
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-class ResourceParser(Parser):
-    exensions = [".jpg", ".png",".gif", ".css", ".html"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path, source, dest):
         self.copy(path, source, dest)
--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 class Site:
     def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
@@ -10,26 +11,22 @@ class Site:
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
-    def build(self):
-        self.dest.mkdir(parents=True, exist_ok=True)
-
-        for path in self.source.rglob("*"):
-            if path.is_dir():
-                self.create_dir(path)
-            elif path.is_file():
-                self.run_parser(path)
-
     def load_parser(self, extension):
-        for p in self.parsers:
-            if p.valid_extension(extension):
-                return p
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
 
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
-
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
             print("Not Implemented")
 
-
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
